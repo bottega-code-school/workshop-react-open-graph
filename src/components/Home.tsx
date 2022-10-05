0b7b81205313b417959bfebdf0d3e712119a23a9
Jordan Hudgens <jordan@devcamp.com>
2022-10-05 15:08:03 -0700

    Built out starter

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,11 @@
 import * as React from "react";
 import Layout from "./Layout";
-import { OpenGraph } from "./OpenGraph";
-
-const isValidUrl = (url: string) => {
-  try {
-    return Boolean(new URL(url));
-  } catch (e) {
-    return false;
-  }
-};
 
 export default function Home() {
   const [url, setUrl] = React.useState("");
-  const [ogUrl, setOgUrl] = React.useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e?.preventDefault();
-
-    if (url?.length > 0 && isValidUrl(url)) {
-      setOgUrl(url);
-    } else {
-      alert("Please enter a valid URL");
-    }
   };
 
   return (
@@ -37,9 +21,7 @@ export default function Home() {
           <button type="submit">Get Preview</button>
         </form>
 
-        <div className="card-container__preview">
-          <OpenGraph url={ogUrl} />
-        </div>
+        <div className="card-container__preview">Preview here...</div>
       </div>
     </Layout>
   );
